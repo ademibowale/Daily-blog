12162abcf8b5edcba8e78426f46cd4fffc33d417
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-18 01:37:42 +0100

    Create MongooDb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
-import  Express from "express";
+ import  express from "express";
+ import mongoose from "mongoose";
+
+ // Connect to MongoDB
+ mongoose.connect("mongodb+srv://dailypeopleblog:Akafakosi@40@dailypeopleblog.ajpiiex.mongodb.net/dailypeopleblog?retryWrites=true&w=majority").then(()=>{console.log("Connected is conneted");}
+
+ );
 
 const app = Express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000 !!");
+  console.log("Server is running on port 3000 !");
 });
