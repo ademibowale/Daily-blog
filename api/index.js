0bf9cfdb5e8a423609fd33879925a0b3867c7eb2
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-18 02:56:12 +0100

    connect db

--- api/index.js
+++ api/index.js
@@ -3,12 +3,12 @@
 
  
  // Connect to MongoDB
- mongoose.connect("mongodb+srv://dailypeopleblog:Akafakosi@40@dailypeopleblog.ajpiiex.mongodb.net/dailypeopleblog?retryWrites=true&w=majority")
-.then(()=>{console.log("MongooDb is conneted");})
-.catch((err)=>{console.log(err);});
+  mongoose.connect("mongodb+srv://dailypeople:Akafakosi@dailypeople.rwxubqy.mongodb.net/dailypeople?retryWrites=true&w=majority")
+ .then(()=>{console.log("MongooDb is conneted");})
+ .catch((err)=>{console.log(err);});
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000 !");
+  console.log("Server is running on port 3000");
 });
