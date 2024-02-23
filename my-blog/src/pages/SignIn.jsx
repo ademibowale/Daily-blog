490d4a714f76259d2f9f1c2da761e66198e19e35
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-23 09:26:11 +0100

    Implement Google authentication

--- my-blog/src/pages/SignIn.jsx
+++ my-blog/src/pages/SignIn.jsx
@@ -1,15 +1,13 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch,useSelector } from 'react-redux';
-// import OAuth from '../components/OAuth';
-import { signInSuccess,signInStart,signInFailure } from '../redux/user/userSlice'; 
-
+import { useDispatch, useSelector } from 'react-redux';
+import OAuth from '../components/OAuth';
+import { signInSuccess, signInStart, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
-const [formData, setFormData] = useState({});
-  const {loading,error: errorMessage}= useSelector(
-    state=>state.user);
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
@@ -30,7 +28,6 @@ const [formData, setFormData] = useState({});
       const data = await res.json();
       if (data.success === false) {
         dispatch(signInFailure(data.message));
-        // return setErrorMessage(data.message);
       }
       dispatch(signInSuccess(data));
       if (res.ok) {
@@ -46,42 +43,45 @@ const [formData, setFormData] = useState({});
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-20'>
         {/* left side */}
         <div className='flex-1'>
-          <Link to="/" className='font-bold dark:text-white text-2xl'>
-            <span className='px-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white '>DiwuraEssential's</span>
+          <Link to='/' className='font-bold dark:text-white text-2xl'>
+            <span className='px-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white '>
+              DiwuraEssential's
+            </span>
             Blog
           </Link>
           <p className='px-1 py-5 text-sn-5'>
-            Hey there! Welcome to DiwuraEssential, join the community– 
-            The social blog where real people talk about real life(and have a blast doing it!) ✨
-            and everything in between!
-            Join the fun! DiwuraEssential: Where good vibes and great conversations flow.
+            Hey there! Welcome to DiwuraEssential, join the community– The social blog where real people talk about real
+            life(and have a blast doing it!) ✨ and everything in between! Join the fun! DiwuraEssential: Where good vibes
+            and great conversations flow.
           </p>
         </div>
-         
+
         {/* right side */}
         <div className=' flex-1'>
           <h2 className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white '>
-            Signin with your email and password or with Google</h2>
-          <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
-            
+            Signin with your email and password or with Google
+          </h2>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='email'/>
-              <TextInput type='email' 
+              <Label value='email' />
+              <TextInput
+                type='email'
                 placeholder='Enter your email'
                 id='email'
                 onChange={handleChange}
-                 />
+              />
             </div>
             <div>
-              <Label value='password'/>
-              <TextInput  type='password' 
+              <Label value='password' />
+              <TextInput
+                type='password'
                 placeholder='**********'
                 id='password'
-                onChange={handleChange}/>
+                onChange={handleChange}
+              />
             </div>
-            <Button  gradientDuoTone='purpleToPink'
-             type='submit' disabled={loading}>
-               {loading ? (
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {loading ? (
                 <>
                   <Spinner size='sm' />
                   <span className='pl-3'>Loading...</span>
@@ -89,21 +89,23 @@ const [formData, setFormData] = useState({});
               ) : (
                 'Sign In'
               )}
-        </Button>
+            </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Dont have an account Signup?</span>
-            <Link to='/sign-up' className='text-blue-500'>Sign Up</Link>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign Up
+            </Link>
           </div>
           {/* Error alert message */}
           {errorMessage && (
             <Alert className='mt-5' color='failure'>
               {errorMessage}
             </Alert>
-            )
-          }
+          )}
         </div>
-      </div> 
+      </div>
     </div>
   );
 }
