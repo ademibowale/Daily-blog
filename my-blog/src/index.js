3c81333f6cc312b5928d4f8a6feba7bc647e2eee
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-23 16:05:28 +0100

    Implement darkmood unctionality

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
 import { store , persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.jsx';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <PersistGate persistor={persistor}>
 <Provider store={store}>
+<ThemeProvider>
     <App />
+    </ThemeProvider>
     </Provider>
     </PersistGate>
 );
--- my-blog/src/redux/store.js
+++ my-blog/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 import { persistReducer, persistStore } from 'redux-persist';
+import themeReducer from './theme/themeSlice';
 import storage from 'redux-persist/lib/storage';
 
 
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
