efba1de16fc6a9e861b4c7e8f7285844cc0422ae
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-25 10:56:19 +0100

    Implement api route update profile functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import {errorHandler} from '../utils/error.js';
+
 
 export const test = (req, res) => {
   res.json({ message: "Api is running !..." });
