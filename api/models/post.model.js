ef407622637fccc497d6e9bac28d591f2eed15b3
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-27 09:08:12 +0100

    Implemet Api route for creat post

---
+++ api/models/post.model.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.schema(
+
+    {
+userId:{
+    type:String,
+    required: true,
+},
+
+content:{
+    type:String,
+    required:true,
+},
+
+title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  image: {
+    type: String,
+    default:
+      'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+  },
+
+  category: {
+    type: String,
+    default: 'uncategorized',
+  },
+
+
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+},
+{ timestamps: true }
+
+
+)
