6ff019e9540e140e566e75787acea0231435c647
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-27 13:52:50 +0100

    Create post dash

--- my-blog/src/components/DashSidebar.jsx
+++ my-blog/src/components/DashSidebar.jsx
@@ -12,7 +12,9 @@ import { useSelector } from 'react-redux';
 function DashSidebar() {
     const location = useLocation()
     const dispatch = useDispatch();
-    const[tab, setTab] = useState('')
+    const { currentUser } = useSelector((state) => state.user);
+    const [tab, setTab] = useState('');
+    
     useEffect(() => {
     const urlParams = new URLSearchParams(location.search)
     const tabFromUrl = urlParams.get('tab');
@@ -40,12 +42,24 @@ function DashSidebar() {
   return (
     <Sidebar className='w-full md:56'>
     <Sidebar.Items>
-      <Sidebar.ItemGroup>
+      <Sidebar.ItemGroup className='flex flex-col gap-1'>
+      {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
         <Link to='/dashboard?tab=profile'>
       <Sidebar.Item
       active={tab === 'profile'}
       icon={HiUser}
-      label={'user'}labelColor='dark' as='div'>
+      label={currentUser.isAdmin ? 'Admin' : 'User'}
+      labelColor='dark' as='div'>
       Profile
      </Sidebar.Item>
         </Link>
@@ -55,9 +69,25 @@ function DashSidebar() {
                 icon={HiDocumentText}
                 as='div'
               >Post</Sidebar.Item>
+               </Link>
+          )}
+          {currentUser.isAdmin && (
+            <>
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                >
+                  Users
+                </Sidebar.Item>
+                </Link>
+
+                
      <Sidebar.Item
       icon={HiArrowSmRight}
-      className='cursor-pointer' onClick={handleSignout}>
+      className='cursor-pointer' 
+      onClick={handleSignout}>
     Sign Out
      </Sidebar.Item>
     </Sidebar.ItemGroup>
