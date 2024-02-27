61f89653a21a234da5f0bb12e6145383262e7f4d
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-27 13:32:38 +0100

    Dashboard-Post-Section

--- my-blog/src/components/DashSidebar.jsx
+++ my-blog/src/components/DashSidebar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Sidebar} from 'flowbite-react';
-import {HiUser,HiArrowSmRight} from 'react-icons/hi';
+import {HiUser,HiArrowSmRight,HiDocumentText,HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,} from 'react-icons/hi';
 import{useEffect,useState} from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function DashSidebar() {
     const location = useLocation()
@@ -46,6 +49,12 @@ function DashSidebar() {
       Profile
      </Sidebar.Item>
         </Link>
+        <Link to='/dashboard?tab=posts'/>
+        <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >Post</Sidebar.Item>
      <Sidebar.Item
       icon={HiArrowSmRight}
       className='cursor-pointer' onClick={handleSignout}>
