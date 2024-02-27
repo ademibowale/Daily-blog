0f92c6b5938af767e06f896061920cff473245dd
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-27 22:58:04 +0100

    Implement update functionality

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -26,7 +26,7 @@ function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
-          {/* <Route path='/update-post/:postId' element={<UpdatePost />} /> */}
+          <Route path='/update-post/:postId' element={<UpdatePost />} /> 
         </Route>
         <Route path='/projects' element={<Projects />} />
         {/* <Route path='/post/:postSlug' element={<PostPage />} /> */}
--- my-blog/src/pages/UpdatePost.jsx
+++ my-blog/src/pages/UpdatePost.jsx
@@ -8,21 +8,45 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useParams } from 'react-router-dom';
 
-export default function CreatePost() {
+export default function UpdatePost() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
   const [publishError, setPublishError] = useState(null);
-
+const {postId} =useParams();
   const navigate = useNavigate();
 
-  const handleUpdloadImage = async () => {
+useEffect(()=>{
+    try {
+        const fetchPost = async () => {
+          const res = await fetch(`/api/post/getposts?postId=${postId}`);
+          const data = await res.json();
+          if (!res.ok) {
+            console.log(data.message);
+            setPublishError(data.message);
+            return;
+          }
+          if (res.ok) {
+            setPublishError(null);
+            setFormData(data.posts[0]);
+          }
+        };
+  
+        fetchPost();
+      } catch (error) {
+        console.log(error.message);
+      }
+    }, [postId]);
+
+
+
+const handleUpdloadImage = async () => {
     try {
       if (!file) {
         setImageUploadError('Please select an image');
@@ -61,7 +85,7 @@ export default function CreatePost() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('/api/post/create', {
+        const res = await fetch(`/api/post/updatepost/${formData._id}/${currentUser._id}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -84,7 +108,7 @@ export default function CreatePost() {
   };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Update a post</h1>
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
@@ -96,13 +120,15 @@ export default function CreatePost() {
             onChange={(e) =>
               setFormData({ ...formData, title: e.target.value })
             }
+            value={formData.title}
           />
           <Select
             onChange={(e) =>
               setFormData({ ...formData, category: e.target.value })
             }
+            value={formData.category}
           >
-           <option value='uncategorized'>Select a category</option>
+            <option value='uncategorized'>Select a category</option>
             <option value='entertainment'>Entertainment</option>
             <option value='relationship talks'>Relationship Talks</option>
             <option value='comedy'>comedy</option>
@@ -144,6 +170,7 @@ export default function CreatePost() {
         )}
         <ReactQuill
           theme='snow'
+          value={formData.content}
           placeholder='Write something...'
           className='h-72 mb-12'
           required
@@ -152,7 +179,7 @@ export default function CreatePost() {
           }}
         />
         <Button type='submit' gradientDuoTone='purpleToPink'>
-          Publish
+          updatepost
         </Button>
         {publishError && (
           <Alert className='mt-5' color='failure'>
