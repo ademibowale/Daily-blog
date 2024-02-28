956c500290104dd98afe795462cb3afbc5b1b9ba
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-28 10:28:25 +0100

    Implement comments section

--- my-blog/src/components/CommentSection.jsx
+++ my-blog/src/components/CommentSection.jsx
@@ -1,8 +1,94 @@
 import React from 'react'
 import { UseSelector, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import Comment from './Comment';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+
 
 function CommentSection(postId) {
-    const {currentUser} = useSelector(state=>state.user)
+    const {currentUser} = useSelector(state=>state.user);
+    const [comment, setComment] = useState('');
+  const [commentError, setCommentError] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200) {
+      return;
+    }
+    try {
+      const res = await fetch('/api/comment/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({
+            content: comment,
+            postId,
+            userId: currentUser._id,
+          }),
+        });
+
+        const data = await res.json();
+      if (res.ok) {
+        setComment('');
+        setCommentError(null);
+        setComments([data, ...comments]);
+      }
+    } catch (error) {
+      setCommentError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    getComments();
+  }, [postId]);
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+
+
+
+
+
+
   return (
     <div>
         { currentUser?
