97f882e9a7514367b98f09cd37800c287f58f612
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-29 12:46:33 +0100

    Add Dashcomp  post

--- my-blog/src/components/DashboardComp.jsx
+++ my-blog/src/components/DashboardComp.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  HiAnnotation,
+  HiArrowNarrowUp,
+  HiDocumentText,
+  HiOutlineUserGroup,
+} from 'react-icons/hi';
+
 
 function DashboardComp() {
+    const [users, setUsers] = useState([]);
+  const [comments, setComments] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [totalUsers, setTotalUsers] = useState(0);
+    const [totalPosts, setTotalPosts] = useState(0);
+    const [totalComments, setTotalComments] = useState(0);
+    const [lastMonthUsers, setLastMonthUsers] = useState(0);
+    const [lastMonthPosts, setLastMonthPosts] = useState(0);
+
   return (
     <div className='p-3 md:mx-auto'>
       <div className='flex-wrap flex gap-4 justify-center'>
@@ -85,7 +103,68 @@ function DashboardComp() {
                 </Table.Body>
               ))}
           </Table>
-  )
+          </div>
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
+          <div className='flex justify-between  p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent comments</h1>
+            <Button outline gradientDuoTone='purpleToPink'>
+              <Link to={'/dashboard?tab=comments'}>See all</Link>
+            </Button>
+          </div>
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>Comment content</Table.HeadCell>
+              <Table.HeadCell>Likes</Table.HeadCell>
+            </Table.Head>
+            {comments &&
+              comments.map((comment) => (
+                <Table.Body key={comment._id} className='divide-y'>
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell className='w-96'>
+                        <p className='line-clamp-2'>{comment.content}</p>
+                    </Table.Cell>
+                    <Table.Cell>{comment.numberOfLikes}</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              ))}
+          </Table>
+          </div>
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
+          <div className='flex justify-between  p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent posts</h1>
+            <Button outline gradientDuoTone='purpleToPink'>
+              <Link to={'/dashboard?tab=posts'}>See all</Link>
+            </Button>
+          </div>
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+            </Table.Head>
+            {posts &&
+              posts.map((post) => (
+                <Table.Body key={post._id} className='divide-y'>
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell>
+                      <img
+                        src={post.image}
+                        alt='user'
+                        className='w-14 h-10 rounded-md bg-gray-500'
+                      />
+                    </Table.Cell>
+                    <Table.Cell className='w-96'>{post.title}</Table.Cell>
+                    <Table.Cell className='w-5'>{post.category}</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              ))}
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+
 
 export default DashboardComp
